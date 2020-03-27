cfb3be8ad94b89719036fc051890fef6b14ee7da
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-03-27 21:37:27 +0700

    add convert rows to Invoices

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 import xlrd
 
+from src.invoice import Invoice
+
+
+def read_file(file_path: str) -> list:
+    read_book = xlrd.open_workbook(file_path, on_demand=True)
+    sheet = read_book.sheet_by_index(0)
+    return [sheet.row_values(r) for r in range(1, sheet.nrows)]
+
+
+def convert(rows_list: list) -> list:
+    return [Invoice(row) for row in rows_list]
+
+
+def copy(file_path1: str, file_path2: str):
+    print(file_path1)
+    print(file_path2)
+
 
 if __name__ == '__main__':
-
-    source_filename = "../resources/Исходник.xlsx"
-    read_book = xlrd.open_workbook(source_filename, on_demand=True)
-
-    sheet = read_book.sheet_by_index(0)  # Читаем из первого листа
-    val = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
-
-    for elem in val[1:]:
-        print(elem)
+    source_filepath = "../resources/Исходник.xlsx"
+    rows = convert(read_file(source_filepath))
