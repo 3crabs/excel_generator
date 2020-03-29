af08ace49fb5ff91f461ca1ba21fdd3a062f6e22
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-03-29 13:34:55 +0700

    read date

--- src/main.py
+++ src/main.py
@@ -10,6 +10,16 @@ def read_file(file_path: str) -> list:
 
 
 def make_invoice_file(invoice: list, template_file_path: str, save_dir_path: str):
+    date = xlrd.xldate_as_tuple(float(invoice[2]), 0)
+    if date[2] < 10:
+        date2str = "0" + str(date[2])
+    else:
+        date2str = str(date[2])
+    if date[1] < 10:
+        date1str = "0" + str(date[1])
+    else:
+        date1str = str(date[1])
+    invoice[2] = date2str + "." + date1str + "." + str(date[0])
     new_file_path = save_dir_path + "/" + invoice[1] + ".xlsx"
     copyfile(template_file_path, new_file_path)
     wb = openpyxl.load_workbook(filename=new_file_path)
